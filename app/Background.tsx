351198902a4f91460e2821582aefc8c911ea8ac1
Soundar Murugan <sm185711@ncr.com>
2022-11-08 22:59:37 +0530

    added placeholder for sections

---
+++ app/Background.tsx
@@ -0,0 +1,31 @@
+export default function Background() {
+	return (
+		<div className='flex justify-center space-x-20 mt-40'>
+			<span className='text-blue-500 font-medium tracking-widest'>
+				BACKGROUND
+			</span>
+			<div className='font-extralight max-w-2xl space-y-8'>
+				<p>
+					I'm currently an Engineer at Upstatement building things for
+					the web with some awesome people. I recently graduated from
+					Northeastern University after completing three awesome
+					six-month co-ops at MullenLowe U.S., Starry, and Apple
+					Music.
+				</p>
+				<p>
+					As a software engineer, I enjoy bridging the gap between
+					engineering and design — combining my technical knowledge
+					with my keen eye for design to create a beautiful product.
+					My goal is to always build applications that are scalable
+					and efficient under the hood while providing engaging,
+					pixel-perfect user experiences.
+				</p>
+				<p>
+					When I'm not in front of a computer screen, I'm probably
+					snowboarding, cruising around on my penny board, or crossing
+					off another item on my bucket list.
+				</p>
+			</div>
+		</div>
+	)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,20 @@ import '../styles/globals.css'
 import Header from './Header'
 import ThemeToggle from './ThemeToggle'
 import Footer from './Footer'
+import Background from './Background'
+import Skills from './Skills'
+import Experience from './Experience'
+import Projects from './Projects'
 
 export default function HomePage() {
 	return (
 		<>
 			<ThemeToggle />
 			<Header />
+			<Background />
+			<Skills />
+			<Experience />
+			<Projects />
 			<Footer />
 		</>
 	)
